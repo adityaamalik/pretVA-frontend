9a58c371f71a3fa9cecda6b348dec6be9001358b
Aditya Malik <aditya007@outlook.com>
2020-10-08 21:49:23 +0530

    project setup and navbar added

---
+++ src/components/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './navbar';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default Main;
---
+++ src/components/navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from '../imgs/logo.png';
+import '../css/navbar.css';
+
+function Navbar() {
+  return (
+    <div>
+        <div className="row justify-content-between m-3">
+        <div className="col-3"><img src={logo} alt="LOGO" height="50px" width="auto"/></div>
+        <div className="col-6 lead">
+            <ul id="navList" className="mt-2">
+                <li><button className="btn">About</button></li>
+                <li className="ml-4"><button className="btn">Services</button></li>
+                <li className="ml-4"><button className="btn btn-outline-warning active">Search</button></li>
+                <li className="ml-4"><button className="btn">Feedback & Support</button></li>
+            </ul>
+        </div>
+        <div className="col-3">
+            <div id="logoutsection" className="mt-2">
+                <i class="fa fa-bell-o" aria-hidden="true"></i>
+                <button className="btn">My Account</button>
+                <button className="btn btn-outline-dark active">Logout</button>
+            </div>
+        </div>
+        </div>
+        <hr id="bottomline"></hr>
+    </div>
+  );
+}
+
+export default Navbar;
